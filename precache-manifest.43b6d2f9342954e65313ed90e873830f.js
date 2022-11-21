65511c6a2e924e6f94ce170a1055be9ff518aff2
Tobi Toyo <toyocodes@gmail.com>
2022-11-20 19:32:03 -0800

    Updates

---
+++ precache-manifest.43b6d2f9342954e65313ed90e873830f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "30dd3bded03daaee8621",
+    "url": "/github.io/smartDistributionApp/static/css/main.1b9a1ebc.chunk.css"
+  },
+  {
+    "revision": "30dd3bded03daaee8621",
+    "url": "/github.io/smartDistributionApp/static/js/main.86535f12.chunk.js"
+  },
+  {
+    "revision": "9a50fffb2ba9f0f8020f",
+    "url": "/github.io/smartDistributionApp/static/js/runtime~main.faf9a63e.js"
+  },
+  {
+    "revision": "14bc06e369d17b860e51",
+    "url": "/github.io/smartDistributionApp/static/js/2.095e9196.chunk.js"
+  },
+  {
+    "revision": "578342f31539406383ef",
+    "url": "/github.io/smartDistributionApp/static/js/3.243e8d68.chunk.js"
+  },
+  {
+    "revision": "ab7b8c4a9fcc2895d3698e9cd7805c55",
+    "url": "/github.io/smartDistributionApp/index.html"
+  }
+];
